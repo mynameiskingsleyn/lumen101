f06642587faab47cc4bab48c3b44f24b4f47113f
Kingsley Nwankwo <kingsley.nwankwo@tulip.io>
2021-10-15 12:46:19 -0400

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    //
+    public function index()
+    {
+      return Post::all();
+    }
+
+    public function store(Request $request)
+    {
+      try{
+        $post = new Post();
+        $post->title = $request->title;
+        $post->body = $request->body;
+        if($post->save()){
+          return response()->json(['status'=>'success','message'=>'Post created successfully!!']);
+        }
+      }catch(\Exception $e){
+        return response()->json(['status'=>'error', 'message'=>$e->getMessage()]);
+      }
+    }
+
+    public function update(Request $request, $id)
+    {
+      try{
+        $post = Post::findOrFail($id);
+        $post->title = $request->title;
+        $post->body = $request->body;
+        if($post->save()){
+          return response()->json(['status'=>'success','message'=>'Post Updated successfully!!']);
+        }
+      }catch(\Exception $e){
+        return response()->json(['status'=>'error', 'message'=>$e->getMessage()]);
+      }
+    }
+
+    public function destroy(Request $request, $id)
+    {
+      try{
+        $post = Post::findOrFail($id);
+        if($post->delete()){
+          return response()->json(['status'=>'success','message'=>'Post deleted successfully!!']);
+        }
+      }catch(\Exception $e){
+        return response()->json(['status'=>'error', 'message'=>$e->getMessage()]);
+      }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->group(['prefix'=>'api'], function() use($router){
+  $router->get('/posts',['as'=>'posts','uses'=>'PostController@index']);
+  $router->post('/create_post',['as'=>'createPost','uses'=>'PostController@store']);
+  $router->put('/post/{id}',['as'=>'updatePost','uses'=>'PostController@update']);
+  $router->delete('/post/{id}',['as'=>'destroyPost','uses'=>'PostController@destroy']);
+});
